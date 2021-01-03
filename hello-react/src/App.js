a55917abe262b669b369746e3780720d54064814
unknown <sand8594@hufs.ac.kr>
2021-01-03 18:39:48 +0900

    Fragment

---
+++ hello-react/src/App.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+
+function App() {
+  return(
+    <Fragment> {/* 안 써도 가능 */}
+      <h1>React</h1>
+      <h2>fragment use</h2>
+    </Fragment>
+  );
+}
+
+export default App;

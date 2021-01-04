9031448f9f0da29096527fc0c018d66b9c6ac9c9
unknown <sand8594@hufs.ac.kr>
2021-01-04 19:16:14 +0900

    class_component

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,14 +1,11 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  const name = '리액트';
-  return(
-    <div>
-      <div className = "react">{name}</div>
-      <input/>
-    </div>
-  );
+class App extends Component{
+  render(){
+    const name = 'react';
+    return <div className = "react">{name}</div>
+  }
 }
 
 export default App;

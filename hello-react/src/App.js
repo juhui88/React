6d6a21086b0b222057c9c01a6aaea97be511b74c
unknown <sand8594@hufs.ac.kr>
2021-01-05 22:27:06 +0900

    이벤트핸들링

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,9 +2,10 @@ import React, {Component} from 'react';
 import MyComponent from './MyComponent';
 import Counter from './Counter';
 import Say from './Say'
+import EventPractice from './EventPractice'
 
 const App = () => {
-  return <Say />
+  return <EventPractice/>
 };
 
 export default App;

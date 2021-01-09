270d93374535817b33756a2dfbb4fdd8dea00cf3
unknown <sand8594@hufs.ac.kr>
2021-01-09 23:18:12 +0900

    ref 달기

---
+++ hello-react/src/ScrollBox.js
@@ -0,0 +1,31 @@
+import Preact, { Component} from 'react';
+
+class ScrollBox extends Component {
+    scrollToBottom = () => {
+        const { scrollHeight , clientHeight} = this.box;
+        this.box.scrollTop = scrollHeight - clientHeight;
+    }
+    render() {
+        const style = {
+            vorder: '1px solid black',
+            height: '300px',
+            width: '300px',
+            overflow: 'auto',
+            position: 'realtive'
+        };
+
+        const innerStyle = {
+            width: '100px',
+            height: '650px',
+            backgraound: 'linear-gradient(white, black)'
+        }
+        return (
+            <div
+                style = {style}
+                ref={(ref) => {this.box=ref}}>
+                    <div style={innerStyle}/>
+                </div>
+        );
+    }
+}
+export default ScrollBox;

2262815aa1c062c411eeeb7eb9d6ac48fd3414fa
unknown <sand8594@hufs.ac.kr>
2021-01-13 01:45:31 +0900

    useState 복습

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -1,38 +1,17 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 
-class Counter extends Component{
-    state = {
-        number: 0,
-        fixedNumber: 0
-        // state초기값 설정
-    };
+const Counter =() => {
+    const [value, setValue] = useState(0);
 
-    render() {
-        const {number, fixedNumber} = this.state; //state조화할때 this.state로 조회
-        return (
-            <div>
-                <h1>{number}</h1>
-                <h2>바뀌지 않는 값: {fixedNumber}</h2>
-                <button
-                //OnClick을 통해 버튼이 클릭되었을 때 호출할 함수 지정
-                onClick = {() => {
-                    this.setState(
-                        {
-                            number:number + 1
-                        },
-                        () => {
-                            console.log('방금 setState가 호출되었습니다.');
-                            console.log(this.state);
-                        }
-                    )
+    return (
+        <div>
+            <p>
+                현재 카운터 값은 <b>{value}</b>입니다.
+            </p>
+            <button onCLick={() => setValue(value +1)}>+1</button>
+            <button onCLick={() => setValue(value -1)}>-1</button>
+        </div>
+    );
+};
 
-                    }}
-                >
-                    +1
-                </button>
-            </div>
-        )
-    }
-    
-}
 export default Counter;

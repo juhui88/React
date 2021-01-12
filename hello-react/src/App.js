5654d0857588b3202b6f8093ec9e58b6cf5f3ef9
unknown <sand8594@hufs.ac.kr>
2021-01-13 01:53:41 +0900

    useEffect

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import MyComponent from './MyComponent';
 import Counter from './Counter';
 import Say from './Say';
@@ -12,7 +12,20 @@ import Info from './Info'
 
 
 const App =() => {
-  return <Info />;
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+        >
+          {visible ? '숨기기' : '보이기'}
+        </button>
+        <hr />
+        {visible && <Info />}
+    </div>
+  );
 };
 
 export default App;

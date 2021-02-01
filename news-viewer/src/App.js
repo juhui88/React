a49736112379cc29d1039c002761c7ae0de9a9f7
unknown <sand8594@hufs.ac.kr>
2021-02-01 18:41:29 +0900

    외부 API 연동

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,27 +1,9 @@
-import React, {useState} from 'react'
-import axios from 'axios';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async() => {
-    try {
-      const response = await axios. get(
-        'https://jsonplaceholder.typicode.com/todos/1',
-      )
-      setData(response.data);
-    } catch(e) {
-      console.log(e);
-    }
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data,null,2)}readOnly={true} />}
-    </div>
-  )
-}
+  return <Route path="/:category?" component={NewsPage} />;
+};
 
 export default App;

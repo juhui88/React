e7f2da235ec2b3b7fc5c52732d60b6465fb3a71a
unknown <sand8594@hufs.ac.kr>
2021-02-06 23:42:58 +0900

    react-redux-tutorial 컨테이너 컴포넌트 만들기

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -13,7 +13,8 @@ const store = createStore(rootReducer, composeWithDevTools());
 ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider> ,
-  document.getElementById('root'));
+  </Provider>,
+  document.getElementById('root'),
+);
 
 serviceWorker.unregister();
--- react-redux-tutorial/src/modules/index.js
+++ react-redux-tutorial/src/modules/index.js
@@ -1,4 +1,4 @@
-import {combineReducer} from 'redux'
+import {combineReducers} from 'redux'
 import counter from './counter'
 import todos from './todos'
 

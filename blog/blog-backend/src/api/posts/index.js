b73fe108a2f345260f64d5879fb1cda00611d2a2
unknown <sand8594@hufs.ac.kr>
2021-02-22 21:44:44 +0900

    22장 데이터 생성과 조회

--- blog/blog-backend/src/api/posts/index.js
+++ blog/blog-backend/src/api/posts/index.js
@@ -3,12 +3,10 @@ import * as postsCtrl from './posts.ctrl';
 
 const posts = new Router();
 
-
 posts.get('/',postCtrl.list);
 posts.post('/',postsCtrl.write);
 posts.get('/:id',postsCtrl.read);
 posts.delete('/:id',postsCtrl.remove);
-posts.put('/:id',postsCtrl.replace);
 posts.patch('/:id',postsCtrl.update);
 
-module.export = posts;
+export default posts;

3e227aebeccc19a85f99f352d1685b0d311217ec
unknown <sand8594@hufs.ac.kr>
2021-02-22 21:50:23 +0900

    22장 데이터 삭제와 수정

--- blog/blog-backend/src/api/posts/post.ctrl.js
+++ blog/blog-backend/src/api/posts/post.ctrl.js
@@ -50,7 +50,39 @@ export const read = async ctx => {
     ctx.throw(500, e);
   }
 };
-
-export const remove = ctx => {};
-
-export const update = ctx => {};
+/*
+  DELETE / api / posts/ :id
+*/
+export const remove = async ctx => {
+  const {id} = ctx.params;
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204; // No Content (성공하기는 했지만 응답할 데이터는 없음)
+  } catch(e) {
+    ctx.throw(500, e);
+  }
+};
+/*
+  PATCH /api/ posts/:id 
+  {
+    title: '수정',
+    body: '수정 내용',
+    tags: ['수정', '태그']
+  }
+*/
+export const update = async ctx => {
+  const {id} = ctx.params;
+  try {
+    const post =  await Post.findByIdAndUpdate(id, ctx.request.body,{
+      new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
+      // false일 때는 없데이트되기 전의 데이터를 반환합니다
+    }).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};

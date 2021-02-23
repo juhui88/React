009dc042ef8393fc41633b91de37594798dc958d
unknown <sand8594@hufs.ac.kr>
2021-02-23 14:44:00 +0900

    22장 요청 검증

--- blog/blog-backend/src/api/posts/post.ctrl.js
+++ blog/blog-backend/src/api/posts/post.ctrl.js
@@ -1,6 +1,16 @@
 import Post from '../../models/post'
-import posts from './index';
+import mongoose from 'mongoose';
+import Joi from '@hapi/joi';
 
+const {ObjectId} = mongoose.Types;
+export const checkObjectId = (ctx, next) => {
+  const {id} = ctx.params;
+  if(!OvjectId.isValid(id)) {
+    ctx.status = 400; // Bad Request
+    return;
+  }
+  return next();
+}
 /*
   POST / api / posts
   {
@@ -10,6 +20,22 @@ import posts from './index';
   }
 */
 export const write = async ctx => {
+  const schema = Joi.object().keys({
+    // 객체가 다음 필드를 가지고 있음을 검증
+    title: Joi.string().required(), // required()가 있으면 필수 항목
+    body: Joi.string().required(),
+    tags: Joi.array()
+      .items(Joi.string())
+      .required(), // 문자열로 이루어진 배열
+  })
+  
+  // 검증하고 나서 검증 실패인 경우 에러 처리
+  const result = schema.validate(ctx.request.body);
+  if(result.error) {
+    ctx.sttatus = 400; // Bad Request
+    ctx.body = result.error;
+    return;
+  }
   const {title, body, tags} = ctx.request.body;
   const post = ew Post({
     title,
@@ -72,6 +98,21 @@ export const remove = async ctx => {
 */
 export const update = async ctx => {
   const {id} = ctx.params;
+  // write에서 사용한 schema와 비슷한데, required()가 없습니다.
+  const schema = Joi.object().keys({
+    title: Joi.string(),
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  });
+
+  // 검증하고 나서 검증 실패인 경우 에러 처리
+  const result = schema.validate(ctx.request.body);
+  if(result.error) {
+    ctx.status = 400; // Bad Request
+    ctx.body = result.error;
+    return;
+  }
+  
   try {
     const post =  await Post.findByIdAndUpdate(id, ctx.request.body,{
       new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
